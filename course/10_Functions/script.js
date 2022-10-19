8d51f48102908556e885c2e5f99587d46e5c4aad
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-10-19 21:33:56 +0200

    Closer look at functions

--- course/10_Functions/script.js
+++ course/10_Functions/script.js
@@ -78,3 +78,151 @@ console.log(passenger);
 //==============================================================================
 //## Functions accepting callback functions
 //==============================================================================
+// function that returns a string without space and lowercase
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+// function that transforms the first word upper
+const upperFirstWord = function (str) {
+  const [firstWord, ...otherWords] = str.split(" "); //NB rest pattern
+  return [firstWord.toUpperCase(), ...otherWords].join(" "); //NB spread operator
+};
+
+//= higher order function
+const transformer = function (str, fn) {
+  const result = fn(str);
+  console.log(
+    `The original string: ${str} was transformed into: ${result} by the function: ${fn.name}`
+  );
+  return result;
+};
+
+// transformer is the higher order function
+transformer("JavaScript is the best!", upperFirstWord); // uppreFirstWord is the callback function
+transformer("JavaScript is the best!", oneWord); // oneWord is the callback function
+
+//NB in this case the callback function is also called "event handler" or "event listener"
+const high5 = function () {
+  console.log("HIGH FIVEEEE 👏");
+};
+document.body.addEventListener("click", high5);
+
+//in arrays, forEach is a callback (we will see it later in the course)
+["Jonas", "Marta", "Adam"].forEach(high5);
+
+// IMPORTANT why callback functions are useful?
+// 1. we can split the code in more reusable parts
+// 2. they allow us to create abstractions
+// ABSTRACITONS = we hide the detail of some code implementation because we don't really care about all that detail
+// and this allows us to think about problems at a higher and more abstract level
+// e.g. the transformer functino (the higher order function) does not care at all about how the string is transformed,
+// it does not care about all that detail: basically it is delegating the string transformation to the other lower level functions
+
+//==============================================================================
+//## Functions returning functions: used in FUNCTIONAL PROGRAMMING
+//==============================================================================
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet("Hey");
+// greet returns a function, therefore if we store the result of greet in a variable, we can use that variable (which is actually a function)
+// as a function, giving it the needed parameter
+// NOTE this is CLOSURE: a complex mechanism that is part of JavaScript
+greeterHey("Elisa");
+
+// NB instead of storing the result in a variable, you can directly call the inner function
+greet("Ciaoooo")("Franco");
+
+const arrowGreet = (greeting) => (name) => console.log(`${greeting} ${name}`);
+arrowGreet("Ciaoooo")("Franco");
+
+//==============================================================================
+//## THE CALL AND APPLY METHODS
+//==============================================================================
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  // book: function()
+  book(flightNum, passengerName) {
+    console.log(
+      `${passengerName} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    // we want the book method to add a new object to our bookings
+    this.bookings.push({
+      flight: `${this.iataCode}${flightNum}`,
+      passengerName,
+    });
+  },
+};
+
+lufthansa.book(239, "Elisa Battistoni");
+lufthansa.book(643, "Franco Conci");
+
+console.log(lufthansa);
+
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+const book = lufthansa.book;
+// like this it is a regular function call (not a method) therefore if you call it you will get an error because the this keyword points to undefined (in strict mode)
+// book(23, "Sarah");
+
+lufthansa.book(111, "Cristina Pedroncelli");
+console.log(lufthansa);
+
+//IMPORTANT we need to explicitly/manually tell JavaScript what the this keyword should be like:
+// if we want to book a Lufthansa flight, the this keyword should point to lufthansa
+// but if you want to book an Eurowings flight, the this keyword should point to eurowings
+// how?
+// there are 3 function methods to do this:
+//# call, apply, bind
+
+//#::::::::::::::::
+//= CALL
+//#::::::::::::::::
+//NB a function is just an object, and objects have methods, therefore functions can have methods too (the call method is one of them)
+// in the call method, the first argument is what we want the this keyword to point to, then the rest of the arguments
+// the call method call the book function with the this keyword set to eurowings
+book.call(eurowings, 444, "Sara Grimaldi"); // all the arguments after the first one are simply the arguments of the original function
+console.log(eurowings);
+
+book.call(lufthansa, 777, "Mary Poppins");
+
+const swiss = {
+  airline: "Swiss Air Line",
+  iataCode: "LX",
+  bookings: [],
+};
+
+book.call(swiss, 693, "Grieke Gelderoos");
+console.log(swiss);
+
+//#::::::::::::::::
+//= APPLY
+//#::::::::::::::::
+// similar method to the call method
+// it does exactly the same thing; the only difference is that
+// apply does not receive a list of arguments after the this keyword, it takes an array of arguments
+const flightData = [438, "Marco Bianchi"];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+// best practice This apply method is not used anymore in modern JavaScript, because now we have a better way of doing the same thing
+book.call(swiss, ...flightData);
+
+//#::::::::::::::::
+//= BIND
+//#::::::::::::::::
+// bind does not immediately call the function: it returns a new function where the this keyword is bound (where the this keyword is set to the input of bind)
+// (it is set to whatever value we pass into bind)
+// this makes it easier if you have to run the function multiplt times
+// i.e. instead of having to use a call all the time, we can just bind once
---
+++ prediction_views.py
@@ -0,0 +1,107 @@
+from django.shortcuts import render
+from django.views.generic import View
+from rest_framework import status
+
+from btk.app.notifications import NotificationManager
+from btk.portal.engine_ai import connect_ws_run_inference
+from btk.portal.forms import JudgementPredictionForm
+from btk.authentication.mixins import LawyersAndAdminAuthMixin
+
+
+class JudgementPredictionFormView(LawyersAndAdminAuthMixin, View):
+    """
+    Class-Based View that shows the form Judgement Prediction
+    (Predizione Sentenza)
+
+    Args:
+        LawyersAndAdminAuthMixin
+        View
+    """
+
+    def get(self, request):
+        """Get form and render template"""
+
+        form = JudgementPredictionForm()
+
+        nmanager = NotificationManager()
+        notifications, unread = nmanager.get_notification(request.user.id)
+
+        # define what goes in the page
+        page_rendering = {
+            "form": form,
+            "notifications": notifications,
+            "unread": unread,
+            "errors": []
+        }
+
+        return render(
+            request,
+            "pages/judgement_prediction/predict_form.html",
+            page_rendering,
+        )
+
+
+class JudgementPredictionResultView(LawyersAndAdminAuthMixin, View):
+    """
+    Class-Based View that shows the results of the 
+    judgement prediction algorithm
+
+    Args:
+        LawyersAndAdminAuthMixin
+        View
+    """
+    def post(self, request):
+        """Handle form submit and run inference"""
+
+        data_request = request.POST.dict()
+        request_host = request.get_host()
+
+        # connect to ws judgement prediction to run inference & get results
+        response_json = connect_ws_run_inference(data_request, request_host)
+        if response_json['http_status_code'] == status.HTTP_200_OK:
+            # prepare data to pass to the rendering
+            form_fields = [i for i in data_request.keys() if "token" not in i]
+            features_importance = response_json["result"]["model_info"]["fields"]["features_importance"]
+            features_model = [i['feature'] for i in features_importance]
+            missing_features = [i for i in form_fields if i not in features_model]
+            for i in missing_features:
+                features_importance.append({"feature":i,"importance":0.0})
+            for idx,val in enumerate(features_importance):
+                features_importance[idx]["feature"] = " ".join(val["feature"].split("_")).upper()
+
+            predicted_class = " ".join(response_json["result"]["predicted_class"].split("_")).upper()
+            predicted_class_proba = round((response_json["result"]["predicted_class_proba"])*100)
+            predicted_class_proba_str = str(predicted_class_proba)+"%"
+
+            # what goes in the page
+            page_rendering = {
+                "predicted_class": predicted_class,
+                "predicted_class_proba_str": predicted_class_proba_str,
+                "predicted_class_proba": predicted_class_proba,
+                "features_importance": features_importance,
+                "msg":response_json["result"]["msg"]
+            }
+
+            return render(
+                request,
+                "pages/judgement_prediction/prediction_results.html",
+                page_rendering,
+            )
+
+        else:  # if response_json['http_status_code'] == status.HTTP_400_BAD_REQUEST:
+            notification_manager = NotificationManager()
+            notifications, unread = notification_manager\
+                .get_notification(request.user.id)
+            form = JudgementPredictionForm()
+            page_rendering = {
+                "form": form,
+                "notifications": notifications,
+                "unread": unread,
+                "errors": response_json['errors']
+            }
+
+            return render(
+                request,
+                "pages/judgement_prediction/predict_form.html",
+                page_rendering,
+            )
