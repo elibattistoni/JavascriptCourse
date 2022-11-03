f0270236e725864ad8d6186908435ec2c796fa8f
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-11-03 10:31:38 +0100

    End Mapty app

--- course/15_Mapty/script.js
+++ course/15_Mapty/script.js
@@ -12,6 +12,7 @@ class Workout {
   // this is cutting edge javascript; not supported everywhere, if you want to be safe do it in the constructor
   date = new Date();
   id = (Date.now() + "").slice(-10); // NB in the real world it is best practice to use a library for creating unique IDs, you should not do it on your own custom like here
+  clicks = 0;
 
   constructor(coords, distance, duration) {
     // this.date = new Date()
@@ -27,6 +28,11 @@ class Workout {
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`;
+  }
+
+  // we can interact with each of the object using the public interface
+  click() {
+    this.clicks++;
   }
 }
 
@@ -73,16 +79,24 @@ class Cycling extends Workout {
 // ·············································································
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
 
   constructor() {
+    //# Get user's position
     this._getPosition();
 
+    //# Get data from local storage
+    this._getLocalStorage();
+
+    //# Attach event handlers
     // we want the event listeners to be added at the beginning, when the script loads
     // and the constructor is the method that automatically gets called as soon as we instantiate app
     form.addEventListener("submit", this._newWorkout.bind(this)); // you have to call bind because otherwise the this keyword will point to the form instead of the object itself
     inputType.addEventListener("change", this._toggleElevationField); // do not need to bind because toggleElevationField does not call the this keyword
+    // move the map on the clicked marker with EVENT DELEGATION
+    containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
   }
 
   _getPosition() {
@@ -108,7 +122,7 @@ class App {
 
     const coords = [latitude, longitude];
     // console.log(this);
-    this.#map = L.map("map").setView(coords, 13);
+    this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
 
     L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       attribution:
@@ -117,6 +131,11 @@ class App {
 
     // Handling clicks
     this.#map.on("click", this._showForm.bind(this)); // the binded this keyword is always the App object
+
+    // render the markers now that the map is available
+    this.#workouts.forEach((work) => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -191,17 +210,25 @@ class App {
     }
 
     //# 5. Add new object to workout array
+    // we delegate the functionality to another method
     this.#workouts.push(workout);
     console.log(this.#workouts);
 
     //# 6. Render workout on map as marker
+    // we delegate the functionality to another method
     this._renderWorkoutMarker(workout);
 
     //# 7. Render workout on list
+    // we delegate the functionality to another method
     this._renderWorkout(workout);
 
     //# 8. Hide form + clear input fields
+    // we delegate the functionality to another method
     this._hideForm();
+
+    //# 9. Set local storage to all workouts
+    // we delegate the functionality to another method
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -274,6 +301,102 @@ class App {
     }
     form.insertAdjacentHTML("afterend", html);
   }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest(".workout"); // .closest() is the opposite of .querySelector()
+    // the element that we are looking for is the one with class workout
+    // so wherever the click happens (no matter if it is inside one of the child divs o spans) we will end up in the li element with the workout class
+    // because from the element that is clicked, we will move up to this exact element using the closest method
+    // console.log("listening here");
+    // console.log(workoutEl);
+    // in this element we have the id, and we will use this id to find the workout in the workouts array
+    // the id serves as a bridge between the user interface and the data in our application
+    if (!workoutEl) return; // if you click not on one of the elements, it logs to null
+    const workout = this.#workouts.find(
+      (work) => work.id === workoutEl.dataset.id
+    );
+    // console.log("LISTENED");
+    // console.log(workout);
+    // take the coordinates from this element and move the map to that position (in leaflet there is a emthod for that)
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+
+    // use the publicj interface
+    // workout.click(); // NB removed because with local storage you do not preserve the prototype chain
+  }
+
+  // NB when you convert the object to a string, and then back to an object, you loose the prototype chain
+  // NB so when you retrieve the objects, they are regular objects, and no longer objects that were created by the running or by the cycling class (and therefore they will not inherit the methods, e.g. the click method)
+  _setLocalStorage() {
+    // local storage is a simple key value store
+    // JSON.stringify() converts any object in JS to a string
+    // best practice: use it only for small amount of data because localStorage is a very simple API (because localStorage is "blocking")
+    localStorage.setItem("workouts", JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    // egt data and convert the string back to the objects with JSON.parse()
+    const data = JSON.parse(localStorage.getItem("workouts"));
+    console.log(data); // inspect: you will see that the prototype chain is gone, the obejcts are not reconducted anymore to Running or Cycling
+    // check if there is some data
+    if (!data) return;
+    //NB to avoid the porblem of losing the prototype chain, we couls loop over the data array and restore the objects by creating a new object using the class, based on the data that comes from the local storage
+    // NB try to do it on your own (for now, I will disable the clicks functionality)
+    // restore workouts array
+    this.#workouts = data;
+    this.#workouts.forEach((work) => {
+      this._renderWorkout(work);
+      // this._renderWorkoutMarker(work); // when we try to add this, right at the beginning when the application is first loaded..
+      //the map has actually not been loaded yet (we are trying to add the marke to the map which is still undefined:
+      // this is a first glimple into the nature of Asynchronous JavaScript)
+      // the loading of the application takes some time
+      // you must load the markers only once the map has been loaded: you can put this logic into _loadMap
+    });
+  }
+
+  reset() {
+    // remove the workouts item from the local store
+    localStorage.removeItem("workouts");
+    // reload the page programmatically
+    location.reload(); // location is a bit object that contains a lot of methods and properties in the browser
+  }
 }
 
 const app = new App();
+
+//# move the map on an marker when clicked
+// at the beginning (when loading the page) we don't have the element on which we want to attach the event listener because it has not been created yet
+// therefore we have to do EVENT DELEGATION: add the event handler to the parent element (i.e. the element with class "workout")
+// we do it in the constructor so that the event listener is added right at the beginning
+
+//# count the clicks that happen on each workout in the workouts list
+// we do this just to make the public interface of these classes more complete, because so far we have not called any mthod outside the classes themselves
+// so we are not really using any public interface on any of these object
+// add the property clicks on the Workout class
+
+//# working with local storage: let's use the local storage API in order to make the workout data persist across page reloads
+// the idea is that whenever a workout is added, then all the workouts will be added to the local storage
+// local storage = a place in he browser where we can store data that will stay there even after we close the page
+// the data is basically linked to the URL on which we are using the application
+// whenever the page loads, we will load all the workouts from the local storage and render tem on the map and on the list
+
+//# delete all the workouts at once
+// add a method to the public interface of the class
+// this class is a bit unusual because so far we do not have any public method, so there is no public interface
+// let's create a reset() method that you can use out here or even in the console
+
+//# FINAL CONSIDERATIONS: what we could have implemented (as challenges)
+// ability to edit a workout
+// ability to delete a workout
+// ability to delete all workouts
+// ability to sort workouts by a certain field (e.g. distance)
+// re buld Running and Cycling object coming from Local Storage
+// more realistic error and confirmation messages
+// (hard) ability to position the map to show all the workouts at once
+// (hard) ability to draw lines and shapes instead of just points
+// geocode location from coordinates (Run in Faro, Portugal) (only after asynchronous JS section): you could use a third party API to plug in the coordinates and that would give you back the real location
+// use an API to display weather data for a workout time and place (only after Asynchronous JS section)
