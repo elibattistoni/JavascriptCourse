a108b65abadd4024b72730972cbc2bd3caf82697
ElisaBattistoni <elisa.battistoni@bluetensor.ai>
2022-11-22 11:23:22 +0100

    Sorted controllers in logical order

--- course/18_Forkify_App/src/js/controller.js
+++ course/18_Forkify_App/src/js/controller.js
@@ -143,13 +143,14 @@ const controlAddRecipe = async function (newRecipe) {
 };
 
 const init = function () {
-  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHanlderRender(controlRecipes);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
+
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
-  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 
 init();
